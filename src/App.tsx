c850d33c34371be1e865468f0338845598e150b1
Ameya Keskar <ameyakeskar1999@gmail.com>
2020-05-05 23:42:36 +0530

    setting up a simple, blank navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,34 @@
 import React from 'react';
-import './App.css';
 
 const App = () => {
   return (
     <div className="App">
-     
+     <NavBar>
+       <NavItem icon = "😀" />
+     </NavBar> 
     </div>
+  );
+}
+
+const NavBar = (props: any) => {
+  return (
+
+    <nav className = "navbar">
+      <ul className = "navbar-nav">
+      {props.children}
+      </ul>
+    </nav>
+
+  );
+}
+
+const NavItem = (props: any) => {
+  return (
+    <li className = "nav-item">
+      <a href = "#" className = "icon-button">
+        {props.icon}
+      </a>
+    </li>
   );
 }
 
